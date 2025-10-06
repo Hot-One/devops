15b37a99ac4ece55d8bce41f2d7918d776419ce9
abdulbositK <abdulbositkabilov1@gmail.com>
2025-10-06 15:37:15 +0500

    init

---
+++ db/postgres/pgpoll/init-primary.sh
@@ -0,0 +1,14 @@
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'secret';
+    SELECT pg_create_physical_replication_slot('replication_slot_1');
+    SELECT pg_create_physical_replication_slot('replication_slot_2');
+EOSQL
+
+echo "host replication replicator 0.0.0.0/0 scram-sha-256" >> "$PGDATA/pg_hba.conf"
+echo "host all all 0.0.0.0/0 scram-sha-256" >> "$PGDATA/pg_hba.conf"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    SELECT pg_reload_conf();
+EOSQL
